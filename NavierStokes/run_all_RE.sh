191c79089310f8382603de3362c94ff52d42c258
Caleb Munger <mungerct@RHIT-R912GJXZ.localdomain>
2025-07-25 21:16:40 -0600

    Ready for parallel processing

--- NavierStokes/run_all_RE.sh
+++ NavierStokes/run_all_RE.sh
@@ -3,8 +3,8 @@
 # Set the constant image path
 img="./InletImages/L_mesh_screenshot2_final.png"
 
-# Loop from 2 to 40
-for i in $(seq 2 40); do
+# Loop from 41 to 100
+for i in $(seq 41 100); do
     echo "Processing input $i with image $img..."
     ./InletBatchScript.py "$i" "$img" 0.5 0.04
 done
